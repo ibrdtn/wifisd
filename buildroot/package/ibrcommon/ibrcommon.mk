1fbc2dd3cdc9cfbbc276d11f8ec24bf0f49c9fa2
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-09-25 19:26:18 +0200

    Fix recipes for latest buildroot

--- buildroot/package/ibrcommon/ibrcommon.mk
+++ buildroot/package/ibrcommon/ibrcommon.mk
@@ -7,7 +7,7 @@ IBRCOMMON_VERSION:=0.10.1
 IBRCOMMON_SOURCE:=ibrcommon-$(IBRCOMMON_VERSION).tar.gz
 IBRCOMMON_SITE:=http://www.ibr.cs.tu-bs.de/projects/ibr-dtn/releases
 IBRCOMMON_LIBTOOL_PATCH:=NO
-IBRCOMMON_DEPENDENCIES:=host-pkg-config libnl openssl
+IBRCOMMON_DEPENDENCIES:=host-pkgconf libnl openssl
 IBRCOMMON_INSTALL_STAGING:=YES
 IBRCOMMON_INSTALL_TARGET:=YES
 
