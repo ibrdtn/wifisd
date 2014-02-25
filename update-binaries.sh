995e2b65a2bde07dc8c73527442fb35603e0ad9d
David Goltzsche <david@goltzsche.net>
2014-02-25 09:42:27 +0100

    removed buffalo jenkins url

--- update-binaries.sh
+++ update-binaries.sh
@@ -3,7 +3,6 @@
 
 # nightlies
 #JENKINS_URL="http://jenkins.ibr.cs.tu-bs.de/jenkins/job/buildroot-ibrdtn-static/defconfig=ka_wifisd_defconfig,label=buildroot-build/lastSuccessfulBuild/artifact/output/target"
-#JENKINS_URL="http://jenkins.ibr.cs.tu-bs.de/jenkins/job/buildroot-ibrdtn-static/defconfig=buffalo_lsgl_defconfig,label=buildroot-build/lastSuccessfulBuild/artifact/output/target"
 
 # stable
 JENKINS_URL="http://jenkins.ibr.cs.tu-bs.de/jenkins/job/ibrdtn-release-buildroot/defconfig=ibrdtn_arm_defconfig,label=moped/lastSuccessfulBuild/artifact/output/target"
