23fd2d7e958b6859184848ad4aac371d8292c6e4
David Goltzsche <david@goltzsche.net>
2014-02-25 16:08:10 +0100

    start_dtnoutbox.sh: more robust

--- sd/boot/start_dtnoutbox.sh
+++ sd/boot/start_dtnoutbox.sh
@@ -1,48 +1,48 @@
 #!/bin/sh
 source /mnt/sd-ext/etc/dtnoutbox.conf
 
-if [ -z $NAME ] || [ -z $OUTBOX ] || [ -z $DESTINATION ]; then
+if [ -z "${NAME}" ] || [ -z "${OUTBOX}" ] || [ -z "${DESTINATION}" ]; then
   echo "error: mandatory option not defined. exiting"
   exit 1
 fi
 
-if [ -z $ENABLED ]; then
-  echo "dtnoutbox not enabled. exiting"
-  exit 0
-else
-  OPTIONS="$NAME $OUTBOX $DESTINATION"
+if [ "${ENABLED}" == 1 ]; then
+  OPTIONS="${NAME} ${OUTBOX} ${DESTINATION}"
 
-  if [ $WORKDIR ]; then
+  if [ -n "${WORKDIR}" ]; then
     OPTIONS="$OPTIONS -w $WORKDIR"
   fi
 
-  if [ $INTERVAL ]; then
+  if [ -n "${INTERVAL}" ]; then
     OPTIONS="$OPTIONS -i $INTERVAL"
   fi
 
-  if [ $ROUNDS ]; then
+  if [ -n "${ROUNDS}" ]; then
     OPTIONS="$OPTIONS -r $ROUNDS"
   fi
 
-  if [ $PATH ]; then
+  if [ -n "${PATH}" ]; then
     OPTIONS="$OPTIONS -p $PATH"
   fi
 
-  if [ $REGEX ]; then
+  if [ -n "${REGEX}" ]; then
     OPTIONS="$OPTIONS -R $REGEX"
   fi
 
-  if [ $INVERT ]; then
+  if [ "${INVERT}" == 1 ]; then
     OPTIONS="$OPTIONS -I"
   fi
 
-  if [ $QUIET ]; then
+  if [ "${QUIET}" == 1 ]; then
     OPTIONS="$OPTIONS -q"
   fi
 
-  if [ $GROUP ]; then
+  if [ "${GROUP}" == 1 ]; then
     OPTIONS="$OPTIONS -g"
   fi
 
   /mnt/sd-ext/bin/dtnoutbox $OPTIONS
+else
+  echo "dtnoutbox not enabled. exiting"
+  exit 0
 fi
