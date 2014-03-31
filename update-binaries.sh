e048312279c77877a00e881b820d072588bbc404
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2014-03-31 16:45:20 +0200

    Release URL of binaries updated

--- update-binaries.sh
+++ update-binaries.sh
@@ -5,7 +5,7 @@
 #JENKINS_URL="http://jenkins.ibr.cs.tu-bs.de/jenkins/job/buildroot-ibrdtn-static/defconfig=ka_wifisd_defconfig,label=buildroot-build/lastSuccessfulBuild/artifact/output/target"
 
 # stable
-JENKINS_URL="http://jenkins.ibr.cs.tu-bs.de/jenkins/job/ibrdtn-release-buildroot/defconfig=ibrdtn_arm_defconfig,label=moped/lastSuccessfulBuild/artifact/output/target"
+JENKINS_URL="http://jenkins.ibr.cs.tu-bs.de/jenkins/job/ibrdtn-release-buildroot/defconfig=ka_wifisd_defconfig,label=buildroot-build/lastSuccessfulBuild/artifact/output/target"
 
 mkdir -p ./sd-ext/sbin
 mkdir -p ./sd-ext/bin
